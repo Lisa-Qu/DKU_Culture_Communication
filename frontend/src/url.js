1d60223746122a5e24f413a8faab17f445b97cbe
Lisha Qu <lisha.qu@dukekunshan.edu.cn>
2025-11-30 16:16:49 +0800

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:5500";
-export const IF = "http://localhost:5500/images/";
+export const URL = "https://dukeks-cultural-exchange-platform.onrender.com";
+export const IF = "https://dukeks-cultural-exchange-platform.onrender.com/images/";
