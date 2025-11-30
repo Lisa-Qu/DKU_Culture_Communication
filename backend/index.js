fbe4d500891cf0fc3000f511317065ad10e9aaf9
Lisha Qu <lisha.qu@dukekunshan.edu.cn>
2025-11-30 17:31:00 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,12 @@ dotenv.config();
 // Middlewares
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: [
-    "http://localhost:5173",                  
-    "https://lisa-qu.github.io"               
-  ], credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+}));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
